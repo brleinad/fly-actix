b5cc91abcc15b1bb93c2131acab18e6a53b92b0b
Daniel Rodas Bautista <daniel.bautista@tivix.com>
2022-04-08 22:31:10 -0400

    feat: create hello world actix app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    format!("Hello World!")
+}
+
+#[actix_web::main] 
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
